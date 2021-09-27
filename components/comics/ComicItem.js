55fccba1ae8113500e711517e51d0e17b03e9ce2
SergioDBA <sergiodaniel0103@gmail.com>
2021-09-26 19:52:59 -0500

    Add comic card component

--- components/comics/ComicItem.js
+++ components/comics/ComicItem.js
@@ -11,18 +11,12 @@ const ComicItem = () => {
     // </li>
 
     <li className="shadow flex flex-col bg-gray-200 rounded-lg m-2">
-      <div className="h-80 bg-gray-400 rounded-lg"></div>
+      <div className="h-72 bg-gray-400 rounded-lg">img</div>
       <div className="flex flex-col items-start mt-4 p-5">
-        <h4 className="text-xl font-semibold">Heading</h4>
-        <p className="text-sm">
-          Some text about the thing that goes over a few lines.
-        </p>
-        <a
-          className="p-2 leading-none rounded font-medium mt-3 bg-gray-400 text-xs uppercase"
-          href="#"
-        >
-          Click Here
-        </a>
+        <h4 className="text-xl font-semibold">Comic Name</h4>
+        <p className="text-sm w-64">Edition</p>
+        <p className="text-sm w-64">Creator</p>
+        <button className="m-5 inline-flex items-center justify-center w-10 h-10 mr-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-full focus:shadow-outline hover:bg-indigo-800"></button>
       </div>
     </li>
   );
